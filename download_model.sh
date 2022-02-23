4c0e9f713ca4aade207d652b18cf3d988a8afa68
a0346f102085fe9f <77968006+a0346f102085fe9f@users.noreply.github.com>
2022-02-23 04:30:38 +0000

    Add download_model.sh
    Just run:
    $ pip install transformers
    $ bash download_model.sh
    
    And you are ready to roll!
    $ python3 argv_inference.py "Hello world"

---
+++ download_model.sh
@@ -0,0 +1,4 @@
+wget http://download-de.europe.naverlabs.com/Splade_Release_Jan22/splade_distil_CoCodenser_large.tar.gz
+tar -xf splade_distil_CoCodenser_large.tar.gz
+mkdir weights
+mv splade_distil_CoCodenser_large weights/
